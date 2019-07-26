80be93222540e34fe865331d5f511f1544c2a7c8
MillicentNj <millicent.njuguna@allangray.co.za>
2019-07-26 23:26:33 +0300

    [feature] add Formik forms

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,140 +1,31 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import UseReactOnlyForm from "./use-react-only/use-react-only";
+import UseFormikForm from "./use-formic-only/use-formic-only";
+
 import "./App.css";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      fullName: "",
-      email: "",
-      age: "",
-      password: "",
-      isInAgreement: false,
-      nationality: "Kenya",
-      gender: ""
-    };
-  }
+const WelcomeToBasicFormApp = () => <h1>Hello there it's react</h1>;
 
-  handleChange = e => {
-    const target = e.target;
-    const value = target.type === "checkbox" ? target.checked : target.value;
-    const name = target.name;
-    this.setState({
-      [name]: value
-    });
-  };
-
-  handleSubmit = event => {
-    alert("This is the data" + this.state.firstName);
-    console.log(">>>", this.state);
-    event.preventDefault();
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">A basic form in react</header>
-        <form className="Form" onSubmit={this.handleSubmit}>
-          <div className="input">
-            <label>
-              Full Name:
-              <input
-                type="text"
-                name="fullName"
-                value={this.state.fullName}
-                onChange={this.handleChange}
-                required
-              />
-            </label>
-          </div>
-          <div className="input">
-            <label>
-              Email:
-              <input
-                type="email"
-                name="email"
-                value={this.state.email}
-                onChange={this.handleChange}
-                required
-              />
-            </label>
-          </div>
-          <div className="input">
-            <label>
-              Age:
-              <input
-                type="number"
-                name="age"
-                value={this.state.age}
-                onChange={this.handleChange}
-                required
-                min={0}
-                max={110}
-              />
-            </label>
-          </div>
-          <div className="input">
-            <label>
-              Nationality:
-              <select
-                name="nationality"
-                value={this.state.nationality}
-                onChange={this.handleChange}
-              >
-                <option value="Kenya">Kenya</option>
-                <option value="Burundi">Burundi</option>
-                <option value="Uganda">Uganda</option>
-                <option value="Rwanda">Rwanda</option>
-              </select>
-            </label>
-          </div>
-          <div className="input">
-            <label htmlFor="gender">gender: </label>
-            <div>
-              <input
-                type="radio"
-                name="gender"
-                value="female"
-                onChange={this.handleChange}
-              />
-              <label>Female</label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                name="gender"
-                value="male"
-                onChange={this.handleChange}
-              />
-              <label>Male</label>
-            </div>
-          </div>
-          <div className="input">
-            <label htmlFor="password">
-              Password:
-              <input
-                type="password"
-                name="password"
-                value={this.state.password}
-                onChange={this.handleChange}
-                required
-              />
-            </label>
-          </div>
-          <div className="input">
-            <input
-              name="isInAgreement"
-              type="checkbox"
-              checked={this.state.isInAgreement}
-              onChange={this.handleChange}
-            />
-            <label>I have agreed to the terms and conditions</label>
-          </div>
-          <input type="submit" className="myButton" />
-        </form>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/use-react-form">React Form</Link>
+      </li>
+      <li>
+        <Link to="/use-formik-form">Formik Form</Link>
+      </li>
+    </ul>
+    <hr />
+    <Route path="/" exact component={WelcomeToBasicFormApp} />
+    <Route path="/use-react-form" component={UseReactOnlyForm} />
+    <Route path="/use-formik-form" component={UseFormikForm} />
+  </Router>
+);
 
 export default App;
